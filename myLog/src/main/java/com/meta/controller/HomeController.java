42eca673596411ee6bb9c52cdc88cbc20f004db9
ShinYangGye <52960744+ShinYangGye@users.noreply.github.com>
2023-05-08 23:33:13 +0900

    수정

--- myLog/src/main/java/com/meta/controller/HomeController.java
+++ myLog/src/main/java/com/meta/controller/HomeController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class HomeController {
 
 	@GetMapping("/home")
 	public String getHome() {
+		log.info("Info #############################");
 		return "Home";
 	}
 	
